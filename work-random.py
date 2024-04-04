64498970f60556ea7dbf1b9bbc74daf638ba1c11
kaelynllee <163168281+kaelynllee@users.noreply.github.com>
2024-04-04 11:18:11 -0700

    work-random.py

--- work-random.py
+++ work-random.py
@@ -1,5 +1,3 @@
 # Author: Kaelyn 
-def random_product(*args, repeat=1):
-    "Random selection from itertools.product(*args, **kwds)"
-    pools = [tuple(pool) for pool in args] * repeat
-    return tuple(map(random.choice, pools))
+import random 
+random.random()
