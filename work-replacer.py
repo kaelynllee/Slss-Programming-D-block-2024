dd0b40bcb48d9d4a71889e8913ff02c1933f1752
Kaelyn Lee <Kaelynkatelee@gmail.com>
2024-04-04 11:57:38 -0700

    Added function notes

--- work-replacer.py
+++ work-replacer.py
@@ -1,29 +1,18 @@
-## Text/Emoji Replacer
+# Emoji Replacer
 # Author: Kaelyn
 # 26 February 2024
 
 
-# Create a function called translate
-#    Accepts a string as parameter
-#    From the parameter replace all 100 with 💯
-#    Also replace all noodles with 🍜
-#    Return the result
 def translate(usr_input):
-    # Your block of code goes in here
-    # Delete the pass and put in your own code
-    return usr_input.replace("100", "💯").replace("noodles", "🍜")
+    return usr_input.replace("1", "1️⃣").replace("bubble tea", "🧋")
 
 
 def main():
-    # Get the user's input
     usr_input = input()
-    # Use the translate function on the
-    #    user's input
     print(translate(usr_input))
 
 
-# Test cases
-print(translate("Get to 100!"))
-print(translate("I like noodles."))
-print(translate("I love 100 noodles."))
+print(translate("I'm number 1 !"))
+print(translate("I love bubble tea."))
+print(translate("I have 1 bubble tea."))
 main()
