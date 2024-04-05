8e3ec1522402a66c0052fb64cf70b3347caa6952
Kaelyn Lee <Kaelynkatelee@gmail.com>
2024-04-05 13:49:17 -0700

    Added to function notes

--- notes-2-2-morefunctions.py
+++ notes-2-2-morefunctions.py
@@ -2,3 +2,22 @@
 # Author: Kaelyn 
 # 4 April 2024 
 
+def stars(num_stars : int) -> str:
+    """Returns a given number of *"""
+    return "*" * num_stars
+value = ""
+
+if num_stars == 0 or num_stars == 1:
+    value = "*"
+elif num_stars > 1:
+else: 
+return value 
+# # Multiply Strings 
+# greeting = "hello"
+# print(greeting * 1_000_000)
+
+# print("The quick brown fox jumps over the laxy dog.")
+print(stars(1)) # "*"
+print(stars(100))
+print(stars(0))
+print(stars(-1))
