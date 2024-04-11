9b81dae2b5f0a9ec9f98df13b2b7de39d1afb44f
Kaelyn Lee <Kaelynkatelee@gmail.com>
2024-04-11 10:59:36 -0700

    updated

--- Unit-mid2-activity.py
+++ Unit-mid2-activity.py
@@ -1,3 +1,29 @@
 # Unit 2 activity
 # Author: Kaelyn 
 # 9 April 2024
+
+def say_hello_params(name):
+    print(f"Hello {name}!")
+
+say_hello_params('Bobita')
+
+def translate(usr_input):
+    return usr_input.replace("You slay", "🫵😍🫵")
+
+def main():
+    usr_input = input()
+    print(translate(usr_input))
+
+print(translate("You slay"))
+main()
+
+user_answer = input ("Can you hang after school?")
+
+while user_answer not in ["yes", "no", "yeah", "nah", "yus","naur"]:
+    user_answer = input ("Do you want to go out to eat?")
+
+if user_answer in ["yes", "yeah","yus"]:
+    print("WOOO let's go get some food!")
+
+if user_answer in  ["no", "nah","not with you", "naur"]:
+    print ("You never have time.")
