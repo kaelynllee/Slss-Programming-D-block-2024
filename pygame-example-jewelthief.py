ad1037bea1a24ca3268aa12bbefd00b5787f9ea9
Kaelyn Lee <Kaelynkatelee@gmail.com>
2024-05-03 09:05:42 -0700

    slaeyed

---
+++ pygame-example-jewelthief.py
@@ -0,0 +1,65 @@
+import pygame as pg
+
+# --CONSTANTS--
+# COLOURS
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+EMERALD = (21, 219, 147)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
+
+WIDTH = 1280  # Pixels
+HEIGHT = 720
+SCREEN_SIZE = (WIDTH, HEIGHT)
+
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        self.image = pg.image.load("./Images/mario.png")
+        self.rect = self.image.get_rect()
+
+
+    def update(self):
+        self.rect.centerx = pg.mouse.get_pos()[0]
+        self.rect.centery = pg.mouse.get_pos()[1]
+def start():
+    """Environment Setup and Game Loop"""
+
+    pg.init()
+
+    # --Game State Variables--
+    screen = pg.display.set_mode(SCREEN_SIZE)
+    done = False
+    clock = pg.time.Clock()
+
+    # All sprites go in this sprite Group
+    all_sprites = pg.sprite.Group()
+
+    pg.display.set_caption("Jewel Theif")
+
+    # --Main Loop--
+    while not done:
+        # --- Event Listener
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+
+        # --- Update the world state
+
+        # --- Draw items
+        screen.fill(BLACK)
+
+        # Update the screen with anything new
+        pg.display.flip()
+
+        # --- Tick the Clock
+        clock.tick(60)  # 60 fps
+
+
+def main():
+    start()
+
+
+if __name__ == "__main__":
+    main()
